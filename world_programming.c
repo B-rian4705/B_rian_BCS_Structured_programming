38b163c3f06fa418a7ecbea6c1c11ac8f6d167f3
B-rian4705 <mwangibrian4705@gmail.com>
2026-09-11 16:37:52 +0300

    Add files via upload
    My C code for task 1.

---
+++ world_programming.c
@@ -0,0 +1,13 @@
+ // Hello world
+/*Brian Mwangi
+BCS-05-0217/2026
+Welcomt to Zetech*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello world\n");
+    printf("Welcome to the world of TECH\n");
+    return 0;
+}
