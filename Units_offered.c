8cbeb8720ea5224c5ff85e8bfee69dd84bcc7472
B-rian4705 <mwangibrian4705@gmail.com>
2026-09-17 14:10:53 +0300

    Add files via upload

---
+++ Units_offered.c
@@ -0,0 +1,15 @@
+// Units offered
+
+int main(){
+	printf("Brian Irungu Mwangi \n");
+	printf("BCS-05-0217/2026 \n");
+	printf("Units offered This semister \n");
+	printf("1. Introduction to programming \n");
+	printf("2. Data structures \n");
+	printf("3. Computer networks \n");
+	printf("4. Database systems \n");
+	printf("5. Software Engineering \n");
+}
+
+
+
